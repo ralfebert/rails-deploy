19668458f3ca754da0f9d5073b0adb32d043be5f
Ralf Ebert <info@ralfebert.de>
2016-05-28 21:30:44 +0200

    use sudo for service restart

--- example-deploy.rb
+++ example-deploy.rb
@@ -89,7 +89,7 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      #queue "service #{user} restart"
+      #queue "sudo service #{user} restart"
     end
   end
 end
