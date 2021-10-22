75b4f89f3b36907dfffcb32aa71ec64da43d1611
Ralf Ebert <info@ralfebert.de>
2021-10-22 23:18:03 +0200

    include public/packs and node_modules as shared_dir

--- example-deploy.rb
+++ example-deploy.rb
@@ -26,6 +26,7 @@ set :rvm_use_path, '/etc/profile.d/rvm.sh'
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 # set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
 set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_dirs, fetch(:shared_dirs, []).push('public/packs', 'node_modules')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
